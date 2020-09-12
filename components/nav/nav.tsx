12439b6ddd11910164efb203935c38056bd6049f
Aleksai Losey <aleksailosey@gmail.com>
2020-09-12 15:36:09 -0400

    Test

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,5 +1,12 @@
 import styles from './nav.module.sass';
 
 export default function Nav() {
-  return <div id={styles.Nav}>Nav bar</div>;
+  return (
+    <div id={styles.Nav}>
+      <div id={styles.NavLeft}>
+        <span>Aleksai Losey</span>
+      </div>
+      <div id={styles.NavRight}></div>
+    </div>
+  );
 }
