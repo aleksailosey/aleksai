10efc588fa48ebf6820fc2b23a3663db3eb19de3
Aleksai Losey <aleksailosey@gmail.com>
2020-09-13 18:07:24 -0400

    update

--- components/area/areaimage.tsx
+++ components/area/areaimage.tsx
@@ -11,23 +11,21 @@ export default function AreaImage({ path, description }) {
   }, []);
 
   return (
-    <div
-      className={styles.AreaImageWrap}
-      style={{
-        backgroundColor: require('../../images' + path + '?lqip-colors')[0]
-      }}
-    >
+    <div className={styles.AreaImageWrap}>
       {loading ? <div className={styles.AreaImageWrapOverlay}></div> : null}
       {!loading ? (
         <div className={styles.AreaImageWrapDescriptionOverlay}>
           <span className={styles.AreaImageDescription}>{description}</span>
         </div>
       ) : null}
+      {process.env.NODE_ENV === 'production' ? (
+        <img
+          className={styles.AreaImage}
+          src={require('../../public/images' + path + '?lqip')}
+        />
+      ) : (
+        <img className={styles.AreaImage} src={`/images${path}`} />
+      )}
     </div>
   );
 }
-
-// <img
-//   className={styles.AreaImage}
-//   src={require('../../images' + path + '?lqip')}
-// />
