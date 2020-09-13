08b0f79e550c9d30392070adaaf025508c6fde6f
Aleksai Losey <aleksailosey@gmail.com>
2020-09-13 16:00:51 -0400

    Type fix

--- contexts/RouterContext.ts
+++ contexts/RouterContext.ts
@@ -1,11 +1,12 @@
 import { createContext } from 'react';
+import { State } from 'types/State';
 
 const PathContext = createContext({
   path: '/',
   pushPath: (path: string) => {},
   replacePath: (path: string) => {},
   state: {},
-  setState: (state: object) => {},
+  setState: (state: State) => {},
   language: 'en',
   setLanguage: (language: string) => {}
 });
